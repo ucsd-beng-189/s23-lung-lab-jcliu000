0d5394395d55daf7557c72572dce5f49bdbe3250
jcliu000 <103136600+jcliu000@users.noreply.github.com>
2023-04-22 22:05:36 -0700

    Task 3: adding loop to vary beta

--- lung.m
+++ lung.m
@@ -2,6 +2,30 @@
 clear all
 clf
 global Pstar cstar n maxcount M Q camax RT cI;
-setup_lung
-cvsolve
-outchecklung
+
+beta_range = 0:0.05:1;   % range to loop over for beta
+
+PI_track = zeros(1,11); % save inspired partial pressure of oxygen
+PA_track = zeros(1,11); % save mean alveolar partial pressure of oxygen
+Pa_track = zeros(1,11); % save mean arterial partial pressure of oxygen
+Pv_track = zeros(1,11); % save venous partial pressure of oxygen
+
+for i = 1:21
+    beta = beta_range(i);
+    setup_lung
+    cvsolve
+    outchecklung
+    PI_track(i) = PI;
+    PA_track(i) = PAbar;
+    Pa_track(i) = Pabar;
+    Pv_track(i) = Pv;
+end
+
+beta_pressures = [PI_track; PA_track; Pa_track; Pv_track];
+
+figure(4)
+plot(beta_range,beta_pressures)
+xlabel('beta')
+ylabel('Pressure (mmHg)')
+title('Partial Pressures of Oxygen vs. Beta')
+legend('Inspired Air','Mean Alveolar Air','Mean Arterial Blood','Venous Blood')
