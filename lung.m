34fd6861a1adf2a8a93bb1183f8707c293f0a427
jcliu000 <103136600+jcliu000@users.noreply.github.com>
2023-04-23 16:54:23 -0700

    Task 8: add loop to vary beta

--- lung.m
+++ lung.m
@@ -3,49 +3,71 @@ clear all
 clf
 global Pstar cstar n maxcount M Q camax RT cI;
 
-cI_frac_range = 0.1:0.1:2;  % range to loop over for multiplier for cI
+beta_range = 0:0.1:1;       % range of beta to loop over
 
-cref=0.2/(22.4*(310/273));  % reference concentration of oxygen in air
+for j = 1:11
+    beta = beta_range(j);
 
-PA_track = zeros(1,20);     % save mean alveolar partial pressures
-cA_track = zeros(1,20);     % save mean alveolar concentrations
-Pa_track = zeros(1,20);     % save mean arterial partial pressures
-ca_track = zeros(1,20);     % save mean arterial concentrations
-Pv_track = zeros(1,20);     % save venous partial pressures
-cv_track = zeros(1,20);     % save venous concentrations
+    cI_frac_range = 0.1:0.1:2;  % range to loop over for multiplier for cI
 
-
-for i = 1:20                % loop for multipliers for cI
-    cI = cref*cI_frac_range(i);
-    setup_lung
-    cvsolve
-    outchecklung
-    PA_track(i) = PAbar;
-    cA_track(i) = cAbar;
-    Pa_track(i) = Pabar;
-    ca_track(i) = cabar;
-    Pv_track(i) = Pv;
-    cv_track(i) = cv;
+    cref=0.2/(22.4*(310/273));  % reference concentration of oxygen in air
+    
+    PA_track = zeros(1,20);     % save mean alveolar partial pressures
+    cA_track = zeros(1,20);     % save mean alveolar concentrations
+    Pa_track = zeros(1,20);     % save mean arterial partial pressures
+    ca_track = zeros(1,20);     % save mean arterial concentrations
+    Pv_track = zeros(1,20);     % save venous partial pressures
+    cv_track = zeros(1,20);     % save venous concentrations
+    
+    
+    for i = 1:20                % loop for multipliers for cI
+        cI = cref*cI_frac_range(i);
+        setup_lung
+        cvsolve
+        outchecklung
+        PA_track(i) = PAbar;
+        cA_track(i) = cAbar;
+        Pa_track(i) = Pabar;
+        ca_track(i) = cabar;
+        Pv_track(i) = Pv;
+        cv_track(i) = cv;
+    end
+    
+    Pressures_track = [PA_track; Pa_track; Pv_track];
+    Concentrations_track = [cA_track; ca_track; cv_track];
+    
+    % calculate elevation
+    elevation = -log(cI_frac_range)/(29/6.022/10^26)/9.8*1.38*10^-23*310;
+    
+    figure(4)   % plot pressures
+    subplot(3,4,j)
+    plot(elevation,Pressures_track)
+    subtitle("beta = " + beta)
+    legend('Mean Alveolar','Mean Arterial','Venous')
+    
+    figure(5)   % plot concentrations
+    subplot(3,4,j)
+    plot(elevation,Concentrations_track)
+    subtitle("beta = " + beta)
+    legend('Mean Alveolar','Mean Arterial','Venous')
 end
 
-Pressures_track = [PA_track; Pa_track; Pv_track];
-Concentrations_track = [cA_track; ca_track; cv_track];
+% making things pretty for the pressures plot
+fig4 = figure(4);
+fig4axes = axes(fig4,'visible','off'); 
+fig4axes.XLabel.Visible = 'on';
+fig4axes.YLabel.Visible = 'on';
+fig4axes.Title.Visible = 'on';
+xlabel(fig4axes,'Elevation (m)');
+ylabel(fig4axes,'Pressure (mmHg)');
+title(fig4axes,'Partial Pressures of Oxygen vs. Elevation')
 
-% calculate elevation
-elevation = -log(cI_frac_range)/(29/6.022/10^26)/9.8*1.38*10^-23*310;
-
-figure(4)   % plot pressures
-plot(elevation,Pressures_track)
-xlabel('Elevation (m)')
-ylabel('Pressure (mmHg)')
-title('Partial Pressures of Oxygen vs. Elevation')
-legend('Mean Alveolar','Mean Arterial','Venous')
-subtitle('Elevation Adaptation')
-
-figure(5)   % plot concentrations
-plot(elevation,Concentrations_track)
-xlabel('Elevation (m)')
-ylabel('Concentration of Oxygen (mol/L)')
-title('Concentrations of Oxygen vs. Elevation')
-subtitle('Elevation Adaptation')
-legend('Mean Alveolar','Mean Arterial','Venous')
+% making things pretty for the concentrations plot
+fig5 = figure(5);
+fig5axes = axes(fig5,'visible','off'); 
+fig5axes.XLabel.Visible = 'on';
+fig5axes.YLabel.Visible = 'on';
+fig5axes.Title.Visible = 'on';
+xlabel(fig5axes,'Elevation (m)');
+ylabel(fig5axes,'Concentration of Oxygen (mol/L)');
+title(fig5axes,'Concentrations of Oxygen vs. Elevation')
--- setup_lung.m
+++ setup_lung.m
@@ -2,7 +2,7 @@
 %heterogeneity parameter (0<=beta<=1):
 %beta=0 for homogenous lung
 %beta=1 for no ventilation/perfusion correlation
-beta=0.5
+%beta=0.5
 %
 %number of iterations used in bisection:
 maxcount=20
@@ -20,7 +20,7 @@ cref=0.2/(22.4*(310/273))
 %
 %blood oxygen concentration
 %at full hemoglobin saturation: 
-cstar=1.5*cref  % adjusted for elevation adaptation
+cstar=cref
 %cstar=4*(concentration of hemoglobin 
 %in blood expressed in moles/liter)
 %
